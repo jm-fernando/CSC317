57c87ffb5a2f6ec2779f08b5bc797f54dee48ee3
jm-fernando <jmfernando@dons.usfca.edu>
2025-11-12 21:04:06 -0800

    calculator.js: Implemented addition

--- assignments/4/scripts/calculator.js
+++ assignments/4/scripts/calculator.js
@@ -2,13 +2,38 @@
 const screen = document.getElementById("calc-screen");
 const buttons = document.querySelectorAll(".buttons button");
 
-//Variable to hold input
+//Variable to hold inputs
 let currentInput = "";
+let previousInput = null;
+let currentOperator = null;
 
 //Loop
 buttons.forEach(function(button) {
     button.addEventListener("click", function() {
         const value = button.textContent;
+
+        //Handling Addition Operator
+        if(button.id === "add") {
+            if(currentInput !== "") {
+                previousInput = parseFloat(currentInput);
+                currentOperator = "+";
+                currentInput = "";
+            }
+            return;
+        }
+
+        //Handling Equals button
+        if(button.id === "equals") {
+            if(currentOperator === "+" && previousInput !== null && currentInput !== "") {
+                let result = previousInput + parseFloat(currentInput);
+                screen.textContent = result;
+                currentInput = result.toString();
+                previousInput = null;
+                currentOperator = null;
+            }
+            return;
+        }
+
 
         //Handling Clear button
         if(button.id === "clear") {
